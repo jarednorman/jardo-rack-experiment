94e4bb19a20d05827f5bb75c070d5f82498c0874
Jared Norman <jared@super.gd>
2021-05-04 19:19:25 -0700

    Add puma config

---
+++ config/puma.rb
@@ -0,0 +1,9 @@
+workers Integer(ENV.fetch('WEB_CONCURRENCY', 2))
+
+threads_count = Integer(ENV.fetch('RAILS_MAX_THREADS', 5))
+threads threads_count, threads_count
+
+preload_app!
+
+port ENV.fetch('PORT', 6969)
+environment ENV.fetch('RACK_ENV', "development")
