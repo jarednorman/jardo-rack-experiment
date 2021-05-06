a51b46125f67fc9cbe6df2d903119fb33e1ae0ba
Jared Norman <jared@super.gd>
2021-05-05 17:52:07 -0700

    I wrote another spec

---
+++ spec/jardo/home_action_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Jardo::HomeAction do
+  describe "#call" do
+    subject { described_class.new.call(request) }
+
+    let(:request) { Rack::Request.new({}) }
+
+    it { is_expected.to eq [200, {"Content-Type" => "text/plain"}, ["Jardo.dev v1"]] }
+  end
+end
