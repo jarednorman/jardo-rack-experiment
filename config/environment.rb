a69356676c738cdfa009a2dfaffc75288890c76e
Jared Norman <jared@super.gd>
2021-05-09 14:33:29 -0700

    Enable reloading in development mode

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,12 @@ require 'bundler'
 
 Bundler.require(:default, ENV["RACK_ENV"])
 
-Zeitwerk::Loader.new.tap do |loader|
-  loader.push_dir File.join(File.dirname(__FILE__), "../src/")
+JARDO_LOADER = Zeitwerk::Loader.new.tap do |loader|
+  loader.push_dir(
+    File.join(File.dirname(__FILE__), "../src/")
+  )
+
+  loader.enable_reloading if ENV["RACK_ENV"] == "development"
   loader.setup
 end
 
