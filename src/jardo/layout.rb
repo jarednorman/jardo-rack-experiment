98477f7698a29473b1b8577b4b5d48c907446a88
Jared Norman <jared@super.gd>
2021-05-09 17:36:53 -0700

    Add another font

--- src/jardo/layout.rb
+++ src/jardo/layout.rb
@@ -9,9 +9,11 @@ module Jardo
             <link rel="preconnect" href="https://fonts.gstatic.com">
             <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap" rel="stylesheet"> 
 
+            <link rel="stylesheet" href="https://use.typekit.net/zjn1exh.css">
+
             <style>
               body {
-                font-family: "JetBrains Mono";
+                font-family: "Usual";
               }
             </style>
           </head>
